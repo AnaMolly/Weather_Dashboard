25ba44edc68a62f53cd7b4ff4b9b86dd484f05e8
AnaMolly <analia.mohamdally@gmail.com>
2021-05-30 18:59:35 -0400

    added variables to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-var APIKey = "24877683e160be4d09b95f62d7d4489b"
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-var city= "toronto"
+var button = document.querySelector(".searchbutton")
+var city = document.querySelector(".locationSearch")
+var location = document.querySelector(".location")
+var temp = document.querySelector(".temp")
+
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=24877683e160be4d09b95f62d7d4489b"
+
 
 fetch(queryURL)
   .then(function (response) {
