5ae1f2836316843e4f39563c46ee7e3840260cf4
AnaMolly <analia.mohamdally@gmail.com>
2021-05-30 18:25:40 -0400

    added fetch to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 var APIKey = "24877683e160be4d09b95f62d7d4489b"
-var city= "toronto"
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
+var city= "toronto"
 
 fetch(queryURL)
   .then(function (response) {
